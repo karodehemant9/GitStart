ddf4c3412d0eee6ba755b7beed1ffa4d4fa61686
karodehemant9 <karode.hemant9@gmail.com>
2023-07-25 23:15:45 +0530

    changing the propertie using DOM

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('I love git');
+//Making the title have a black border
+let headerTitle = document.getElementById('header-title');
+let mainHeader = document.getElementById('main-header');
+headerTitle.style.border = 'solid 3px #000'
+mainHeader.style.border = 'solid 3px #000'
+
+//making ADD ITEM bold and change the font color to green
+let items = document.getElementsByClassName('title');
+items[0].style.fontWeight = 'bold'
+items[0].style.color = 'green'
