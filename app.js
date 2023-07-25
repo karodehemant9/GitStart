b8b936cad52f7edcde5b4ff3dd6afdff945fac07
karodehemant9 <karode.hemant9@gmail.com>
2023-07-25 23:47:20 +0530

    task 3

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ headerTitle.style.border = 'solid 3px #000'
 mainHeader.style.border = 'solid 3px #000'
 
 //making ADD ITEM bold and change the font color to green
-let items = document.getElementsByClassName('title');
-items[0].style.fontWeight = 'bold'
-items[0].style.color = 'green'
+let lists = document.getElementsByClassName('title');
+lists[0].style.fontWeight = 'bold'
+lists[0].style.color = 'green'
+
+
+let items = document.getElementsByClassName('list-group-item');
+console.log(items)
+items[2].style.backgroundColor = 'green'
+
+
+let li = document.getElementsByTagName('li')
+
+for(let i = 0;i<li.length;i++)
+{
+    li[i].style.fontWeight = 'bold'
+    li[i].style.backgroundColor = '#f4f4f4'
+}
