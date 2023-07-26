309d6831d28a163142d2f5a10a840a6a7009db8a
karodehemant9 <karode.hemant9@gmail.com>
2023-07-26 13:31:02 +0530

    adding edit buttons too li

--- app.js
+++ app.js
@@ -1,72 +1,103 @@
-//traversing the DOM
-let itemList = document.querySelector('#items')
-
-//prentNode
-console.log(itemList.parentNode)
-
-//parentElement
-console.log(itemList.parentElement)
-
-//childNode
-console.log(itemList.childNodes)
-
-//childrens
-console.log(itemList.children)
+let form  = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+let filter = document.getElementById('filter');
 
 
-//singlechildren
-console.log(itemList.children[0])
-itemList.children[0].textContent = 'HELLO Item 1'
+//form submit event
+form.addEventListener('submit',addItem);
 
-//firstChild
-console.log(itemList.firstChild)
+//delete event
+itemList.addEventListener('click',removeItem);
 
-//lastChild
-console.log(itemList.lastChild)
+//fiter event
+filter.addEventListener('keyup',filterItems);
 
-//firstElementChild
-console.log(itemList.firstElementChild)
+//Add item
+function addItem(e)
+{
+    e.preventDefault();
 
-
-//lastElementChild
-console.log(itemList.lastElementChild)
-
-//nextSibling
-console.log(itemList.nextSibling)
-
-//previousSibling
-console.log(itemList.previousSibling)
-
-//nextElementSibling
-console.log(itemList.nextElementSibling)
-
-//previousElementSibling
-console.log(itemList.previousElementSibling)
-
-//createElement
-let newDiv = document.createElement('div')
-console.log(newDiv);
-newDiv.className = 'hello';
-newDiv.id = 'hi'; 
-
-//setAttribute
-newDiv.setAttribute('title','Hello Div');
-
-//createTextNode
-let newDivText = document.createTextNode('Hello World');
-
-//adding textNode to Div
-newDiv.appendChild(newDivText); 
+    //get input value()
+    let newItem = document.getElementById('item').value;
 
 
-let container = document.querySelector('header .container');
-let h1 = document.querySelector('header h1');
-container.insertBefore(newDiv,h1);
+    //create new li element
+    let li = document.createElement('li');
+
+    //add class
+    li.className = 'list-group-item';
+
+
+    //add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    //create delete button element
+    let deleteButton = document.createElement('button');
+
+    //add class to delete button
+    deleteButton.className = 'btn btn-danger btn-sm float-right delete';
+
+    //delete button text node
+    deleteButton.appendChild(document.createTextNode('X'));
+
+    //append button to li
+    li.appendChild(deleteButton);
+
+    //append li to list
+    itemList.appendChild(li);
+
+     
+} 
+
+
+//remove item
+function removeItem(e)
+{
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm('Are you sure?'))
+        {
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+
+}
+
+//filter items
+function filterItems(e)
+{
+    //converting to lower case
+    let text = e.target.value.toLowerCase();
+    //get list items
+    let items = itemList.getElementsByTagName('li');
+    //covert collection to an array
+    Array.from(items).forEach(function(item)
+    {
+        let itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1)
+        {
+            item.style.display = 'block';
+        }
+        else
+        {
+            item.style.display = 'none';
+        }
+    });
+
+}
 
 
 
 
-itemList.previousElementSibling.previousElementSibling.previousElementSibling.textContent = 'HELLO Add Items'
+let allItems = document.getElementsByTagName('li');
+console.log(allItems);
 
-
-itemList.children[0].textContent = 'HELLO Item 1'
+for(let i = 0;i<allItems.length;i++)
+{
+    let editButton = document.createElement('button');
+    editButton.className = 'btn btn-primary btn-sm float-right edit';
+    let newButtonText = document.createTextNode('Edit');
+    editButton.appendChild(newButtonText);
+    allItems[i].appendChild(editButton);
+}
